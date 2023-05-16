ff5234082fceda693e16dc6fc72716fc55da743b
bnonni <bryan@nonni.io>
2023-05-16 17:35:57 -0400

    updates to hd wallet generation

--- bitcoin-protocol-development/scripts-wallets/practice/generate_hd_wallet.py
+++ bitcoin-protocol-development/scripts-wallets/practice/generate_hd_wallet.py
@@ -25,31 +25,36 @@ def encode_base58(x):
     while acc:
         acc, idx = divmod(acc, 58)
         x_base58 = ALPHABET[idx: idx + 1] + x_base58
-    
+
     return x_base58
+
 
 # entropy:
 #   generate 128 bits of entropy
 entropy = urandom(ENTROPY_LENGTH // 8)
+print("entropy:", entropy)
+print("entropy_hex:", entropy.hex())
 # entropy_binary:
 #   convert entropy bytes to integer in big endian order
 #   convert big endian integer to bits
 #   drop '0b' from front
 #   pad front with 0s until length of resulting string = full entropy length in bits
 entropy_binary = bin(int.from_bytes(entropy, byteorder="big"))[
-2:].zfill(len(entropy) * 8)
-# print("entropy_binary:", "*" * ENTROPY_LENGTH)
+    2:].zfill(len(entropy) * 8)
+print("entropy_binary:", entropy_binary)
 
 # entropy_hash_256:
 #   generate 256 bit hash using sha256
 #   digest into hex format
 entropy_hash_256 = sha256(entropy).hexdigest()
-# print("entropy_hash:", entropy_hash_256)
+print("entropy_hash:", entropy_hash_256)
 # entropy_hash_256_binary:
 #   convert entropy_hash_256 from hex to integer then integer to bits
 #   drop '0b' from front of bits
 #   pad front with 0s until length of resulting bits = 256
 entropy_hash_256_binary = bin(int(entropy_hash_256, 16))[2:].zfill(256)
+print("entropy_hash_256_binary:", entropy_hash_256_binary)
+
 # checksum_length:
 #   determine the checksum length based on full entropy length in bits
 #   entropy = 128, checksum length = 4
@@ -61,7 +66,7 @@ checksum = entropy_hash_256_binary[:checksum_length]
 # entropy_checksum:
 #   append binary checksum to end of entropy bits
 entropy_checksum_binary = entropy_binary + checksum
-# print("entropy_checksum_bits:", entropy_checksum_binary)
+print("entropy_checksum_binary:", entropy_checksum_binary)
 
 # entropy_checksum_binary_segments:
 #   split the entropy bits + checksum into a list of 11 bit segments
@@ -80,11 +85,11 @@ mnemonic_phrase_bytes = mnemonic_phrase.encode("utf-8")
 passphrase_bytes = passphrase.encode("utf-8")
 PBKDF2_ROUNDS = 2048
 seed_hash_512 = pbkdf2_hmac(
-"sha512", mnemonic_phrase_bytes, passphrase_bytes, PBKDF2_ROUNDS)
-# print("seed_hash_512:", seed_hash_512)
+    "sha512", mnemonic_phrase_bytes, passphrase_bytes, PBKDF2_ROUNDS)
+print("seed_hash_512:", seed_hash_512)
 
 master_extended_private_key = hmac.new(
-b"Bitcoin seed", seed_hash_512, digestmod=sha512).digest()
+    b"Bitcoin seed", seed_hash_512, digestmod=sha512).digest()
 master_private_key = master_extended_private_key[32:]
 master_chain_code = b"\x00" + master_extended_private_key[:32]
 
@@ -93,10 +98,10 @@ depth_pfp_childnum = b"\x00" * 9
 xprv += depth_pfp_childnum
 xprv += master_private_key
 xprv += master_chain_code
-# print("xprv_0:", xprv)
+print("xprv_0:", xprv)
 
 xprv_checksum = sha256(sha256(xprv).digest()).digest()[:4]
-# print("xprv_checksum:", xprv_checksum)
+print("xprv_checksum:", xprv_checksum)
 
 # Append 4 bytes of checksum
 xprv = encode_base58(xprv + xprv_checksum)
